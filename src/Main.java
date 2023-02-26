4454a869c57b10dd966aabd70f4d87c813e580c5
Anh Le <85472773+kubiii1288@users.noreply.github.com>
2023-02-26 23:47:18 +0700

    try some thead features

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ThreadLocal<Integer> threadLocal1 = ThreadLocal.withInitial(() -> 0);
+        ThreadLocal<Integer> threadLocal2 = new ThreadLocal<>();
+
+        threadLocal1.set(1);
+        threadLocal2.set(1);
+
+        threadLocal1.remove();
+        threadLocal2.remove();
+        System.out.println(threadLocal1.get());
+        System.out.println(threadLocal2.get());
     }
 }
